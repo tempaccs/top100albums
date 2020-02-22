b2abd43cce90b1c7947b28a32ff1f56404fcc27e
MS <belvostuff@protonmail.com>
2020-02-22 13:42:45 +0100

    Move from css files to emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Global } from "@emotion/core";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <Global
+      styles={{
+        body: {
+          margin: 0,
+          fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`,
+          "-webkit-font-smoothing": "antialiased",
+          "-moz-osx-font-smoothing": "grayscale"
+        },
+        code: {
+          fontFamily:
+            'source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace'
+        }
+      }}
+    />
+    <App />
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
