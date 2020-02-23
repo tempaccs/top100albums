414c86b5830b2a40cf88f969937eb4ee14e4e4f9
MS <belvostuff@protonmail.com>
2020-02-23 18:12:29 +0100

    Add more tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { render, waitForElementToBeRemoved } from "@testing-library/react";
+import {
+  render,
+  waitForElementToBeRemoved,
+  waitForElement
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("App", () => {
-  test("renders album list and detail page", async () => {
+  test("renders album list", async () => {
     const { getByText, getByTestId } = render(<App />);
     const loadingSpinner = getByTestId("Loading");
     expect(loadingSpinner).toBeInTheDocument();
@@ -26,5 +30,23 @@ describe("App", () => {
     userEvent.type(searchBar, "Linkin Park");
     expect(getAllByTestId("AlbumList-Album").length).toBe(1);
     expect(getByText("Hybrid Theory - LINKIN PARK")).toBeInTheDocument();
+  });
+
+  test("shows details page", async () => {
+    const { getByText, getByTestId } = render(<App />);
+    await waitForElementToBeRemoved(() => getByTestId("Loading"));
+    userEvent.click(getByText("MAP OF THE SOUL : 7 - BTS"));
+    expect(getByText("MAP OF THE SOUL : 7 - BTS")).toBeInTheDocument();
+    expect(getByText("buy for $13.99")).toBeInTheDocument();
+    expect(getByText("K-Pop")).toBeInTheDocument();
+    expect(getByText("February 21, 2020")).toBeInTheDocument();
+    const description = await waitForElement(() =>
+      getByText(node =>
+        node.includes(
+          "Map of the Soul: 7 is the fourth Korean-language (seventh overall) studio album by South Korean boy band BTS."
+        )
+      )
+    );
+    expect(description).toBeInTheDocument();
   });
 });
