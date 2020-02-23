0bd7e881eaf98cc26e6c7c3669f88891cd74a0ea
MS <belvostuff@protonmail.com>
2020-02-23 13:04:20 +0100

    work on detailswq

--- src/data/albums/api.ts
+++ src/data/albums/api.ts
@@ -1,5 +1,3 @@
-type TODO = any;
-
 type AlbumImage = {
   label: string;
   attributes: { height: string };
@@ -16,13 +14,34 @@ export type Album = {
     };
   };
   "im:artist": { label: string };
+  category: {
+    attributes: {
+      label: string;
+    };
+  };
+  "im:releaseDate": {
+    label: string;
+    attributes: {
+      label: string;
+    };
+  };
+  "im:price": {
+    label: string;
+  };
+  link: {
+    attributes: {
+      rel: string;
+      type: string;
+      href: string;
+    };
+  };
 };
 
 export type Feed = {
   entry: Array<Album>;
 };
 
-type Payload = {
+type ItunesResponse = {
   feed: Feed;
 };
 
@@ -30,6 +49,45 @@ export const getTopAlbums = async () => {
   const response = await fetch(
     "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
   );
-  const payload: Payload = await response.json();
+  const payload: ItunesResponse = await response.json();
   return payload.feed;
+};
+
+type WikiArticleResponse = {
+  query: {
+    pages: {
+      [articleId: string]: {
+        extract: string;
+      };
+    };
+  };
+};
+type WikiSearchResults = {
+  query: {
+    search: Array<{
+      snippet: string;
+      pageid: number;
+    }>;
+  };
+};
+export const getAlbumDescription = async (album: Album) => {
+  const searchResponse = await fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${album["im:name"].label}`
+  );
+  const searchResults: WikiSearchResults = await searchResponse.json();
+  const relevantArticle = searchResults.query.search.find(search =>
+    search.snippet
+      .toLowerCase()
+      .includes(album["im:artist"].label.toLowerCase())
+  );
+
+  if (!relevantArticle) {
+    return undefined;
+  }
+
+  const response = await fetch(
+    `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&origin=*&pageids=${relevantArticle.pageid}`
+  );
+  const payload: WikiArticleResponse = await response.json();
+  return Object.values(payload.query.pages)[0].extract;
 };
--- src/screens/AlbumList/index.tsx
+++ src/screens/AlbumList/index.tsx
@@ -1,40 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { getTopAlbums, Feed } from "data/albums/api"; // eslint-disable-line no-unused-vars
+import React, { useState } from "react";
 import Album from "./components/Album";
 import SearchBar from "./components/SearchBar";
 import Centered from "components/Centered";
-import Loading from "components/Loading";
+import { Feed } from "data/albums/api"; // eslint-disable-line no-unused-vars
 
-const AlbumList = () => {
-  const [feed, setFeed] = useState<Feed>();
+type Props = {
+  feed: Feed;
+};
+
+const AlbumList = ({ feed }: Props) => {
   const [searchValue, setSearchValue] = useState<string>("");
-
-  useEffect(() => {
-    getTopAlbums().then(feed => setFeed(feed));
-  }, []);
 
   return (
     <>
       <SearchBar value={searchValue} onChange={val => setSearchValue(val)} />
       <Centered>
-        {!feed ? (
-          <Loading />
-        ) : (
-          feed.entry
-            .filter(album => {
-              if (!searchValue) return true;
-              const searchFor = searchValue.toLowerCase();
-              const searchIn = album.title.label.toLowerCase();
-              return searchIn.includes(searchFor);
-            })
-            .map((album, idx) => (
-              <Album
-                key={album.id.attributes["im:id"]}
-                dark={idx % 2 === 0}
-                album={album}
-              />
-            ))
-        )}
+        {feed.entry
+          .filter(album => {
+            if (!searchValue) return true;
+            const searchFor = searchValue.toLowerCase();
+            const searchIn = album.title.label.toLowerCase();
+            return searchIn.includes(searchFor);
+          })
+          .map((album, idx) => (
+            <Album
+              key={album.id.attributes["im:id"]}
+              dark={idx % 2 === 0}
+              album={album}
+            />
+          ))}
       </Centered>
     </>
   );
